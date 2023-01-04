a8962bd450f9aa6142169e887ba21d52e7abbb60
Samuel Hellstrøm <hellstromsamuel@gmail.com>
2023-01-05 00:06:40 +0100

    Full project
    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef, useState } from "react";
+import "./styles/App.css";
+import { CircularProgress } from "@mui/material";
+import { CoursePage } from "./components/CoursePage";
+import { calculateSemestersFromToday } from "./functions/calculateSemestersFromToday";
+import { calculateCurrentSemesterCode } from "./functions/calculateCurrentSemesterCode";
+import { apiGetCoursesInSemester } from "./uio-api/apiGetCoursesInSemester";
 
-function App() {
+const baseUrl = "https://data.uio.no/studies/v1/course/";
+const currentSemesterCode = calculateCurrentSemesterCode();
+const allSemesters = calculateSemestersFromToday(currentSemesterCode);
+
+const App = () => {
+  const [loading, setLoading] = useState<boolean>(true);
+  const allCoursesUseRef = useRef<{ [courseCode: string]: string[] }>({});
+
+  useEffect(() => {
+    apiGetCoursesInSemester(
+      allCoursesUseRef.current,
+      allSemesters[allSemesters.length - 1].value,
+      allSemesters[0].value,
+      setLoading
+    );
+  }, [allCoursesUseRef]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Søk etter emner på UIO</h1>
+      {loading ? (
+        <div className="loadingContainer">
+          <CircularProgress size={100} />
+        </div>
+      ) : (
+        <CoursePage
+          allAvailableCourses={allCoursesUseRef.current}
+          baseUrl={baseUrl}
+          currentSemesterCode={currentSemesterCode}
+          allSemesters={allSemesters}
+        />
+      )}
     </div>
   );
-}
+};
 
 export default App;
